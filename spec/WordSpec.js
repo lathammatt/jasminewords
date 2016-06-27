9a9beb5c8e99181c8479145c48a0a114c3d38d43
Matt Latham <latham75@gmail.com>
2016-06-27 13:11:25 -0500

    test spec file done

--- spec/WordSpec.js
+++ spec/WordSpec.js
@@ -5,11 +5,29 @@ describe ("word check", function () {
 	});
 
 	it ("should return false if over 100 words", function(){
-			checkWordCount(string){
-				var words = string.split(" ");
-				expect(words.length>100).toBeFalsy();
-			};
+			// checkWordCount(string){
+			// 	var words = string.split(" ");
+				// expect(words.length>100).toBeFalsy();
+			expect(checkWordCount("This is a test string")).toBe(true)();
+			expect(checkWordCount("This is a test string This is a test string This is a test string This is a test string This is a test string This is a test string This is a test string This is a test string This is a test string This is a test string This is a test string This is a test string This is a test string This is a test string This is a test string This is a test string This is a test string This is a test string This is a test string This is a test string This is a test string ")).toBe(false)();
+			// };
 	});
 
+	it ("should have duplicate word checker", function(){
+		expect(duplicateCheck).toBeDefined();
+	});
 
+	it ("return false if string has duplicate words", function(){
+			expect(duplicateCheck("This is a test string")).toBe(true)();
+			expect(duplicateCheck("This is a test test string")).toBe(false)();
+	});
+
+	it ("should have nonalpha linter", function(){
+		expect(verifyAlphaNumeric).toBeDefined();
+	});
+
+	it ("return false if any non-alphanumeric characters", function (){
+		expect(verifyAlphaNumeric("Th15 15 a t35t 5tr1n6")).toBe(true)();
+		expect(verifyAlphaNumeric("Th1$ i$ @ t#s# 5tr!n6:")).toBe(false)();
+	});
 });
